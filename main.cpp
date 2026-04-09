a47554e7689bb6cdea6e49b78ca4d2da8bb26371
Dhruv Patel <dhruvthakorpatel05@gmail.com>
2026-04-09 11:56:25 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <limits>
+
+struct Product 
+{
+    long price;
+    std::vector<string> inputs
+};
+
+std::unordered_map<std::string, Product> products;
+std::unordered_map<std::, long> memo;
+
+long parseCents(const std::string& s)
+{
+
+}
+
+long minCost (const std::string& name)
+{
+    
+}
+
+int main() 
+{
+    return 0;
+}
